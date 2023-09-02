cffeb9bb9d0c107ed635050151bd9d99f91a9316
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-09-02 14:50:58 +0300

    Add bootstrap, css, and homepage main components

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="./style/style.css">
+    <title>CodeFront Developers</title>
+</head>
+
+<body>
+    <h1 id="main_title">CODEFRONT DEVELOPER MANAGEMENT</h1>
+    <div class="container">
+        <h2>All Developers</h2>
+        <table class="table table-hover table-bordered table-striped">
+            <thead>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>3</td>
+                    <td>Robert</td>
+                    <td>Kanyoro</td>
+                    <td>30</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Eliud</td>
+                    <td>Ngugi</td>
+                    <td>30</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+</body>
+
+</html>
